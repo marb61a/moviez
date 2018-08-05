7ed9e8c2874424eebb58680b6fef1420b50ee41b
martin byrne <marb61a@gmail.com>
2018-08-05 20:39:36 +0000

    updated app.js with MultiDataList component

--- src/App.js
+++ src/App.js
@@ -111,6 +111,51 @@ class App extends Component {
               ]
             }}
           />
+          <hr />
+          <MultiDataList 
+            componentId="language-list"
+            dataField="original_language.raw"
+            className="language-filter"
+            title="language"
+            size={100}
+            sortBy="asc"
+            queryFormat="or"
+            selectAllLabel="All Languages"
+            showCheckbox={true}
+            showSearch={true}
+            placeholder="Search for a language"
+            react={{
+              and: [
+                "mainSearch",
+                "results",
+                "date-filter",
+                "RangeSlider",
+                "genres-list",
+                "revenue-list"
+              ]
+            }}
+            data={[
+              {
+                label: "English",
+                value: "English"
+              },
+              {
+                label: "Chinese",
+                value: "Chinese"
+              },
+              {
+                label: "Hindi",
+                value: "Hindi"
+              }
+            ]}
+            showFilter={true}
+            filterLabel="Language"
+            URLParams={false}
+            innerClass={{
+              label: "list-item",
+              input: "list-input"
+            }}
+          />
         </ReactiveBase>
     );
   }
