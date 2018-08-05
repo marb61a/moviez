5c91f2a5896513f8606636188ae28bb4afe0290f
martin byrne <marb61a@gmail.com>
2018-08-05 11:05:22 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactiveBase
+          app="moviez"
+          credentials="qOrtSSiNb:99c511f3-4e81-4047-a6a7-3ed5c4cb0912"
+          theme={{
+            typography: {
+              fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',fontSize: "16px"
+            },
+            colors: {
+              textColor: "#fff",
+              backgroundColor: "#212121",
+              primaryTextColor: "#fff",
+              primaryColor: "#2196F3",
+              titleColor: "#fff",
+              alertColor: "#d9534f",
+              borderColor: "#666"
+            }
+          }}
+         >
+          Hello from ReactiveSearch!
+        </ReactiveBase>
     );
   }
 }
